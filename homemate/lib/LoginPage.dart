43bef60b314d632cba584a01a7dfb2bee621dc06
Mariana Araujo <mariana.a.lucena@accenture.com>
2021-03-22 19:40:31 -0300

    Layout da LoginPage criado

--- homemate/lib/LoginPage.dart
+++ homemate/lib/LoginPage.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,40 +10,98 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(105, 131, 170, 2),//(142, 198, 197, 0.4),
-        title: Text("HOMEMATE"),
-        centerTitle: true,
-      ),
 
-      backgroundColor: Color.fromRGBO(244, 244, 244, 0.9),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-        //  mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.person_outline,
-            ),
+        padding: EdgeInsets.all(30.0),
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
 
-            TextField(
-             // controller: _nameController,
-              // focusNode: _nameFocus,
-              decoration: InputDecoration(labelText: "Nome"),
-              onChanged: (text){},
-            ),
-            TextField(
-             // controller: _emailController,
-              decoration: InputDecoration(labelText: "Senha"),
-              onChanged: (text){},
-              keyboardType: TextInputType.visiblePassword,
-            ),
-          ],
+             Icon(Icons.house_outlined, size: 150, color: Color.fromRGBO(133, 102, 170, 4)),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'Home',
+                  style: TextStyle(color: Color.fromRGBO(133, 102, 170, 4), fontSize: 30, fontWeight: FontWeight.bold),
+                  children: <TextSpan>[
+                    TextSpan(text: 'mate', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(142, 198, 197, 4),)),
+                  ],
+                ),
+              ),
+
+             SizedBox(
+               height: 10,
+             ),
+              Text("Bem-vindo de volta!", textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold,),),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(
+                    color: Color.fromRGBO(105, 131, 170, 2), fontSize: 15),
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                style: TextStyle(
+                    color: Color.fromRGBO(105, 131, 170, 2), fontSize: 15),
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                child: Text("Esqueceu a senha?", style: TextStyle(color: Color.fromRGBO(105, 131, 170, 4)), textAlign: TextAlign.start,),
+              ),
+
+              SizedBox(
+                height: 40,
+              ),
+              RaisedButton(
+                onPressed: () {},
+               child: Container(
+                height: 20,
+                child: Text("ENTRAR", textAlign: TextAlign.center, style: TextStyle(color: Colors.white),),),
+                color: Color.fromRGBO(105, 131, 170, 2),
+                elevation: 3.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1.0),
+                    side: BorderSide(color: Color.fromRGBO(105, 131, 170, 2),)),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'Ainda não tem cadastro? ',
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                  children: <TextSpan>[
+                    TextSpan(text: 'Cadastre-se', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(133, 102, 170, 4),)),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
         ),
-
       ),
     );
-
   }
 }
