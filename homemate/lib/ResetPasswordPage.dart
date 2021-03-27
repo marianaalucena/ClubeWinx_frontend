7aba9c967e8ab431d646f3cc3ce9ce6ec1b36ea3
Mariana Araujo <mariana.a.lucena@accenture.com>
2021-03-27 12:21:09 -0300

    Integração entre as telas de Login e Cadastro

--- homemate/lib/ResetPasswordPage.dart
+++ homemate/lib/ResetPasswordPage.dart
@@ -11,6 +11,8 @@ class ResetSenha extends StatefulWidget {
 
 class _ResetSenhaState extends State<ResetSenha> {
   final _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
 
 
   @override
@@ -30,35 +32,45 @@ class _ResetSenhaState extends State<ResetSenha> {
                 textAlign: TextAlign.center,
                 text: TextSpan(
                   text: 'Home',
-                  style: TextStyle(color: Color.fromRGBO(133, 102, 170, 4), fontSize: 30, fontWeight: FontWeight.bold),
+                  style: TextStyle(color: Color.fromRGBO(133, 102, 170, 4),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
                   children: <TextSpan>[
-                    TextSpan(text: 'mate', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(142, 198, 197, 4),)),
+                    TextSpan(text: 'mate',
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(142, 198, 197, 4),)),
                   ],
                 ),
               ),
               SizedBox(
                 height: 40,
               ),
-              Text("Uma nova senha será enviada para o seu email", style: TextStyle(fontSize: 20),),
+              Text("Uma nova senha será enviada para o seu email",
+                style: TextStyle(fontSize: 20),),
               SizedBox(
                 height: 20,
               ),
-              TextField(
-                controller: _emailController,
-                autofocus: true,
-                keyboardType: TextInputType.visiblePassword,
-                style: TextStyle(
-                    color: Color.fromRGBO(105, 131, 170, 2), fontSize: 15),
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  labelStyle: TextStyle(color: Colors.black),
+              Form(
+                key: _formKey,
+                child: TextFormField(
+                  controller: _emailController,
+                  validator: _validateEmail,
+                  autofocus: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  style: TextStyle(
+                      color: Color.fromRGBO(105, 131, 170, 2), fontSize: 15),
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.black),
+                  ),
                 ),
               ),
               RaisedButton(
-                onPressed: _login,
+                onPressed: _validateInputs,
                 child: Container(
                   height: 20,
-                  child: Text("RECUPERAR A SENHA", textAlign: TextAlign.center, style: TextStyle(color: Colors.white),),),
+                  child: Text("RECUPERAR A SENHA", textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),),),
                 color: Color.fromRGBO(105, 131, 170, 2),
                 elevation: 3.0,
                 shape: RoundedRectangleBorder(
@@ -72,28 +84,37 @@ class _ResetSenhaState extends State<ResetSenha> {
     );
   }
 
-  String _validarEmail(String value) {
+  String _validateEmail(String value) {
     String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regExp = new RegExp(pattern);
     if (value.length == 0) {
-      return "Informe o Email";
-    } else if(!regExp.hasMatch(value)){
+      return "Informe um email válido";
+    } else if (!regExp.hasMatch(value)) {
       return "Email inválido";
-    }else {
+    } else {
       return null;
     }
   }
 
-  void _login(){
+  void _formToJson() {
     Map<String, dynamic> loginUser = Map();
     loginUser["email"] = _emailController.text;
     String json = jsonEncode(loginUser);
 
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => NovaSenha()),
-    );
-
     print(json);
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+  }
+
+  void _validateInputs() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      _formToJson();
+    } else {
+      _autoValidate = true;
+    }
   }
 }
