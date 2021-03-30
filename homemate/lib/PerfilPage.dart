26cfdd4200988ac62545bbb7f50ce9cd5d315c1e
Helena Dantas <helena.dantas@ccc.ufcg.edu.br>
2021-03-29 23:36:13 -0300

    Adicionando descrição no perfil

--- homemate/lib/PerfilPage.dart
+++ homemate/lib/PerfilPage.dart
@@ -20,116 +20,133 @@ class _PerfilPageState extends State<PerfilPage> {
           icon: Icon(Icons.arrow_back_ios),
           onPressed: (){},
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.edit_outlined),
+            onPressed: (){},
+          ),
+        ],
         backgroundColor: Color.fromRGBO(133, 102, 170, 4),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 16),
-            width: MediaQuery.of(context).size.width,
-            height: 300,
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(133, 102, 170, 4),
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(32),
-                bottomLeft: Radius.circular(32)
+      body: SingleChildScrollView(
+        child:
+        Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 16),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/2.5,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(133, 102, 170, 5),
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(32),
+                    bottomLeft: Radius.circular(32)
+                ),
               ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-
-                    Container(
-                      width: 120,
-                      height: 120,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: NetworkImage(url)
-                          )
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16, bottom: 32),
-                  child: Text('Clube Winx',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16, right: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Icon(Icons.email_outlined, color: Colors.white,),
-                          Text('Email',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                        ],
+
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(url)
+                            )
+                        ),
                       ),
-                      Column(
-                        children: <Widget>[
-                          Icon(Icons.group, color: Colors.white,),
-                          Text('Rede Social',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Icon(Icons.phone, color: Colors.white,),
-                          Text('Celular',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                        ],
-                      )
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 20,
-            padding: EdgeInsets.all(42),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 32),
+                    child: Text('Clube Winx',
+                      style: TextStyle(
+                          color: Color.fromRGBO(244, 244, 244, 5),
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Text('Leaders',
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold
-                          ),
+                        Column(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.email_outlined, color: Color.fromRGBO(244, 244, 244, 5),),
+                              onPressed: (){},
+                            ),
+                            Text('Email',
+                              style: TextStyle(
+                                color: Color.fromRGBO(244, 244, 244, 5),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.group, color: Color.fromRGBO(244, 244, 244, 5),),
+                              onPressed: (){},
+                            ),
+                            Text('Rede Social',
+                              style: TextStyle(
+                                color: Color.fromRGBO(244, 244, 244, 5),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.phone, color: Color.fromRGBO(244, 244, 244, 5)),
+                              onPressed: (){},
+                            ),
+                            Text('Celular',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(244, 244, 244, 5),
+                              ),
+                            ),
+                          ],
                         )
                       ],
                     ),
-                  ],
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
-          )
-        ],
+            Container(
+              height: MediaQuery.of(context).size.height/2,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(42),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(244, 244, 244, 5) ,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Text('	Lorem ipsum interdum cursus convallis '
+                      'fringilla dictumst vitae nec venenatis, '
+                      'dictumst platea diam placerat posuere integer '
+                      'ullamcorper torquent, dictumst mauris molestie '
+                      'pulvinar a varius duis potenti. eu aliquam ante '
+                      'metus lacus habitant bibendum urna, ligula '
+                      'himenaeos scelerisque at ad aptent. ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.normal, color: Color.fromRGBO(105, 131, 170, 2))
+                  ),
+                ],
+              )
+            )
+          ],
+        ),
       ),
     );
   }
