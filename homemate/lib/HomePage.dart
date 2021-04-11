2ccebe6c1949e7eb11d70a1d46c4d68a9c0a725f
Mariana Araujo <mariana.a.lucena@accenture.com>
2021-04-11 16:42:54 -0300

    Tela de adicionar anuncio iniciada

--- homemate/lib/HomePage.dart
+++ homemate/lib/HomePage.dart
@@ -1,17 +1,76 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'NewAnnouncementPage.dart';
+import 'ProfilePage.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("HOMEPAGE!!!"),
+      backgroundColor: Color.fromRGBO(244, 244, 244, 5),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color.fromRGBO(133, 102, 170, 4),
+        title: Text("Anúncios"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Center(
+            heightFactor: 6,
+            child: IconButton(
+              iconSize: 80,
+              icon: Icon(Icons.library_add_sharp, color: Color.fromRGBO(133, 102, 170, 4), ),
+              onPressed: _clickButton,
+            ),
+          ),
+        ]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            this.index = index;
+          });
+          switch (index){
+            case 0:  Navigator.of(context).pushNamed('/medical centre');
+            break;
+            case 1:  Navigator.of(context).pushNamed('/medical centre');
+            break;
+            case 2:  Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProfilePage()),
+            );
+            break;
+
+          }
+
+        },
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.house_outlined), title: Text("Inicio")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wc), title: Text("Conexões")),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Perfil"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _clickButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NewAnnouncementPage()),
     );
   }
 }
---
+++ homemate/lib/NewAnnouncementPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewAnnouncementPage extends StatefulWidget {
+  @override
+  _NewAnnouncementPageState createState() => _NewAnnouncementPageState();
+}
+
+class _NewAnnouncementPageState extends State<NewAnnouncementPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(133, 102, 170, 4),
+        title: Text("Novo Anúncio"),
+      ),
+    );
+  }
+}
