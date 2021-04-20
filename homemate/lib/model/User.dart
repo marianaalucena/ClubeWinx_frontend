cabdfbbdcd5ef6705acb1f24951bf09a3e2a154a
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-04-20 01:35:24 -0300

    Integraçao da criaçao de Usuario com o backend

--- homemate/lib/model/User.dart
+++ homemate/lib/model/User.dart
@@ -7,16 +7,16 @@ class User {
   final String gender;
   final String birthDate;
   final String phoneNumber;
-  final String cep;
+  final String zipCode;
   final String street;
   final String number;
-  final String neighborhood;
+  final String district;
   final String complement;
   final String city;
   final String state;
 
   User({@required this.name, @required this.email, @required this.password, this.gender, @required this.birthDate, this.phoneNumber,
-    this.cep, @required this.street, this.number, @required this.neighborhood, this.complement, @required this.city, @required this.state});
+    this.zipCode, @required this.street, this.number, @required this.district, this.complement, @required this.city, @required this.state});
 
   factory User.fromJson(Map<String, String> json) {
     return User(
@@ -24,12 +24,12 @@ class User {
       email: json['email'],
       gender: json['gender'],
       password: json['password'],
-      birthDate: json['birthDate'],
+      birthDate: json['birthday'],
       phoneNumber: json['phoneNumber'],
-      cep: json['cep'],
+      zipCode: json['zipCode'],
       street: json['street'],
       number: json['number'],
-      neighborhood: json['neighborhood'],
+      district: json['district'],
       complement: json['complement'],
       city: json['city'],
       state: json['state']
